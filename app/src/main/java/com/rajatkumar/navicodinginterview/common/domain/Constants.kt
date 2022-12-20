4bba1b9c7ef0cb3e36f30811775421b658bca768
Rajat Kumar <kumar.rajat11114@gmail.com>
2022-12-20 08:15:06 +0530

    Code Cleanup

--- app/src/main/java/com/rajatkumar/navicodinginterview/common/domain/Constants.kt
+++ app/src/main/java/com/rajatkumar/navicodinginterview/common/domain/Constants.kt
@@ -3,7 +3,8 @@ package com.rajatkumar.navicodinginterview.common.domain
 object Constants {
     const val BASE_URL = "https://api.github.com"
 
-    //is:pr author:rajatkumar archived:false is:closed
+    //is:pr author:kumarrajat11114 archived:false is:closed
+    //is:pr list --repo square/retrofit
     const val QUERY = "is:pr list --repo square/retrofit"
 
     const val CREATED = "created"
